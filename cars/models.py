8730e0e0ef9fa56718952473fda97e3fd46204a8
Bryan Akenathon <akeenathon@hotmail.com>
2025-04-29 22:32:05 -0300

    modeling app cars

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=30)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=8, blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+    bio = models.TextField(max_length=600, blank=True, null=True)
+    favorited_by = models.ManyToManyField(User, related_name='favorite_cars', blank=True)
+
+    def __str__(self):
+        return self.model
