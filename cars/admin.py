1d41531c80bfc00015994450db7164dd33bfbe8e
Bryan Akenathon <akeenathon@hotmail.com>
2025-04-29 22:45:41 -0300

    create cars admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Car, Brand
 
-# Register your models here.
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = (
+        'model', 'brand',
+        'factory_year', 'model_year',
+        'plate', 'value',
+    )
+    search_fields = ('name', 'brand',)
